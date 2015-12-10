a70a59ca616fbb48be18fc9fa7b0f42fc824d2ad
Caitlin Stein <caitlin.teter@gmail.com>
2015-12-10 15:19:50 -0600

    added a little extra to the js page just for fun

--- main.js
+++ main.js
@@ -7,3 +7,27 @@ $("#colorBtn").on("click", function(){
   setTimeout(function(){ 
     $("#colorbox").toggleClass("colorbox2"); }, 3000);
 })
+
+//given a little more time, I would probably explore the .delay() and 
+//.queue() jQuery methods
+
+
+//just for fun...
+var williams = '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/YtyrRpGAg34?rel=0" frameborder="0" allowfullscreen></iframe>';
+var mahler = '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/OGZDL8BYotU?rel=0" frameborder="0" allowfullscreen></iframe>';
+var bach = '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/PCicM6i59_I?rel=0" frameborder="0" allowfullscreen></iframe>'
+
+var musician = $(".select").val();
+
+$(".select").change(function(musican) {
+  console.log($(".select").val());
+  var musician = $(".select").val();
+  if(musician == "williams") {
+    $("#youtube").html(williams);
+  } else if (musician == "mahler") {
+    $("#youtube").html(mahler);
+  } else if (musician == "bach") {
+    $("#youtube").html(bach);
+  }
+});
+
